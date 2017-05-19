7b30e2128108f8aac5beb9642947c946ac35cdcd
de <denanokk@gmail.com>
2017-05-19 17:44:02 +0900

    git rm -r --cached

--- INSTALL/open-usp-tukubai-2014061402/deb/usp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-INSTALL_PATH=/opt/usp/bin
-
-if [ -d "$INSTALL_PATH" ];then
-  echo $PATH | grep "$INSTALL_PATH" || export PATH=$PATH:$INSTALL_PATH
-  # echo $PATH | grep "$INSTALL_PATH" || export $INSTALL_PATH:PATH=$PATH
-fi
